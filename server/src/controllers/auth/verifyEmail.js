1b6cd90a308c2b2d67153ee4c82d72eef5242c68
loskaab <165549552+loskaab@users.noreply.github.com>
2024-04-30 17:38:28 +0200

    Update verifyEmail.js

--- server/src/controllers/auth/verifyEmail.js
+++ server/src/controllers/auth/verifyEmail.js
@@ -27,7 +27,9 @@ const verifyEmail = ctrlWrapper(async (req, res) => {
   if (!newUser) throw HttpError(403, `Failed to verify ${user.email}`);
 
   // Log in
-  const session = await Session.create({ uid: newUser._id });
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 2);
+  const session = await Session.create({ uid: newUser._id, expiresAt });
   
   const payload = { uid: newUser._id, sid: session._id };
   const accessToken = jwt.sign(payload, TOKEN_ACCESS_SECRET, { expiresIn: '60s' });
