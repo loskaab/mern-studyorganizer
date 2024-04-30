bc6a71910115f1e687f3aa319c20a4aa926aee72
loskaab <165549552+loskaab@users.noreply.github.com>
2024-04-30 17:40:17 +0200

    Update Session.js

--- server/src/models/Session.js
+++ server/src/models/Session.js
@@ -4,13 +4,10 @@ const { mongooseError } = require('../utils');
 
 const required = [true, 'Required field!'];
 
-const exdate = new Date();
-exdate.setDate(exdate.getDate() + 2);
-
 const sessionSchema = new Schema(
   {
     uid: { type: Schema.Types.ObjectId, required },
-    expiresAt: { type: Date, expires: '2d', default: exdate },
+    expiresAt: { type: Date, expires: 60 * 60 * 24 * 2 },
   },
   { versionKey: false, timestamps: true },
 );
